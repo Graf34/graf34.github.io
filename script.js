f8f0ebe99f839cadb3a09b73ceaaed582d96762e
Graf34 <37001531+Graf34@users.noreply.github.com>
2020-02-25 22:37:39 +0300

    ds
    x

--- script.js
+++ script.js
@@ -2,115 +2,93 @@ var primer = document.getElementById("primer");
 
 var otvet = document.getElementById("otvet");
 
-var a = "";
+var a="";
 
-var b = "";
+var b="";
 
-var x = " ";
+var x=" ";
 
-var otvetNum = "";
+var otvetNum="";
 
-var dota = true;
+var dota=true;
 
-var dotb = true;
-
-
-function getNum(num) {
-    if (otvetNum == "") {
-        if (x == " ") {
-            if (a.length < 10) {
-                a += String(num);
-                otvet.innerText = a;
-            }
-        } else {
-            if (b.length < 10) {
-                b += String(num);
-                otvet.innerText = b;
-            }
-        }
-    } else { location.reload(); }
-
-}
-
-function getOp(op) {
+var dotb=true;
 
 
-    if (!isNaN(parseFloat(a)) && (x == " ")) {
-        x = op;
-        primer.innerText = a + op;
-        otvet.innerText = "";
-    } else {
-        if (a == "" && op == "-") {
-            a += "-";
-            otvet.innerText = a;
-        } else {
-            if (a == "-" && op == "+") {
-                a = "";
-                otvet.innerText = a;
-            }
-        }
-
-        if (b == "" && op == "-") {
-            b += "-";
-            otvet.innerText = b;
-        } else {
-            if (b == "-" && op == "+") {
-                b = "";
-                otvet.innerText = b;
-            }
-        }
+function getNum(num){
+    if (otvetNum==""){
+if (x==" "){
+    if(a.length<10)
+    {
+a += String(num);
+otvet.innerText= a;
     }
+}
+else{
+    if(b.length<10)
+    {
+b += String(num);
+otvet.innerText= b;
+    }
+}
+}else{location.reload();}
 
-
-    //  console.log("X:" + x + ";" + "A:" + a + ";" + "B:" + b);
 }
 
+function getOp(op){
+    if (x==" " && !isNaN(a)){
+        x=op;
+        primer.innerText= a+op;
+        otvet.innerText= "";
+    }
+}
 
-
-
-function rovno() {
-    if (b != "") {
-        primer.innerText = a + x + b;
-        switch (x) {
+function rovno(){
+    if (b!=""){
+        primer.innerText= a+x+b;
+        switch (x){
             case "*":
-                otvetNum = parseFloat(a * b);
-                otvet.innerText = otvetNum;
-                break;
+                otvetNum= parseFloat(a*b);
+                otvet.innerText= otvetNum;
+            break;
             case "-":
-                otvetNum = parseFloat(a - b);
-                otvet.innerText = otvetNum;
-                break;
+                otvetNum= a-b;
+                otvet.innerText= otvetNum;
+            break;
             case "+":
-                otvetNum = parseFloat(a) + parseFloat(b);
-                otvet.innerText = otvetNum;
-                break;
+                otvetNum= a+b;
+                otvet.innerText= otvetNum;
+            break;
             case "/":
-                otvetNum = parseFloat(a / b);
-                otvet.innerText = otvetNum;
-                break;
+                otvetNum= a/b;
+                otvet.innerText= otvetNum;
+            break;
         }
     }
 
 }
 
-function getDot() {
-
-    if (otvetNum == "") {
-        if (x == " ") {
-            if (a.length < 8 && dota) {
-                dota = false;
-                a += ".";
-                otvet.innerText = a;
+function getDot(){
+    
+        if (otvetNum==""){
+            if (x==" "){
+                if(a.length<8 && dota)
+                {
+            dota=false;
+            a += ".";
+            otvet.innerText= a;
+                }
             }
-        } else {
-            if (b.length < 8 && dotb) {
-                dotb = false;
-                b += ".";
-                otvet.innerText = b;
+            else{
+                if(b.length<8 && dotb)
+                {
+            dotb=false;
+            b += ".";
+            otvet.innerText= b;
+                }
             }
-        }
-    } else { location.reload(); }
-
-
-
+            }else{location.reload();}
+            
+            
+    
 }
